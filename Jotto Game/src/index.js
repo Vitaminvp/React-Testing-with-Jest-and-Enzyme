474daf525d2f40ce84c9978c7352a805e5549520
Vitalii Ovcharenko <vitalii.ovcharenko@livestories.com>
2019-07-01 12:57:20 +0300

    added redux

--- Jotto Game/src/index.js
+++ Jotto Game/src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./configureStore";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+//import store from "./configureStore";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+	//<Provider store={store}>
+	<App />,
+	//</Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- Jotto Game/test/testUtils.js
+++ Jotto Game/test/testUtils.js
@@ -1,20 +1,14 @@
-import checkPropTypes from "check-prop-types";
-import rootReducer from "../src/reducers";
-import {createStore} from "redux";
+import checkPropTypes from 'check-prop-types';
+//import rootReducer from "../src/reducers";
+import { createStore } from 'redux';
 
-export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
-};
+// export const storeFactory = initialState => {
+//   return createStore(rootReducer, initialState);
+// };
 
-export const findByAttr = (wrapper, attr) =>
-  wrapper.find(`[data-test='${attr}']`);
+export const findByAttr = (wrapper, attr) => wrapper.find(`[data-test='${attr}']`);
 
 export const checkProps = (component, conformingProps) => {
-  const propError = checkPropTypes(
-    component.propTypes,
-    conformingProps,
-    "prop",
-    component.name
-  );
-  expect(propError).toBeUndefined();
+	const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
+	expect(propError).toBeUndefined();
 };
